1d2c69821f4ccb4adbc96b40bcf8bb8f600eb88d
Eduard Iglesias <eduard.iglesias@cirvianum.cat>
2024-04-30 17:55:52 +0200

    inici poke

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,1 +1,20 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
+async function listMovies(id, name) {
+  try {
+    if (id) {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    } else if (name) {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    } else {
+      alert("necessitem id o nom");
+    }
+    if (!res.ok) {
+      throw new Error("API request failed");
+    }
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
