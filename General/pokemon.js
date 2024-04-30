7a9cd21674c24ac944d4866a87d5db5bae93878f
Eduard Iglesias <eduard.iglesias@cirvianum.cat>
2024-04-30 18:16:08 +0200

    await

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,18 +1,28 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
-async function listMovies(id, name) {
+async function getPokemon(id = 0, name = "") {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   try {
-    if (id) {
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    } else if (name) {
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    } else {
-      alert("necessitem id o nom");
-    }
     if (!res.ok) {
       throw new Error("API request failed");
     }
     const data = await res.json();
     console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+showPokemon();
+
+async function showPokemon() {
+  try {
+    const pokemon = await getPokemon(2);
+
+    console.log(pokemon.abilities);
+
+    // pokemon.abilities.ability.name;
   } catch (error) {
     console.error(error);
     return [];
