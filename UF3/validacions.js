b10233ad9ac62541776a61509c117c21ea0e8101
Eduard Iglesias <eduard.iglesias@cirvianum.cat>
2024-04-30 16:08:28 +0200

    ex1

--- UF3/validacions.js
+++ UF3/validacions.js
@@ -1,7 +1,30 @@
 // A continuació et mostro algunes idees per poder començar a organitzar el teu codi.
 
 // Selecció dels inputs...
-const nameInput = document.querySelector('input[name="name"]');
+const nameInput = document.querySelector("#name");
+const cognomInput = document.querySelector('input[name="surname"]');
+
+document.getElementById("formulari").addEventListener("submit", (e) => {
+  e.preventDefault();
+  validarNomICognom();
+});
+
+function validarNomICognom() {
+  const nameInput = document.querySelector("#name");
+  const cognomInput = document.querySelector('input[name="surname"]');
+  console.log(nameInput.value);
+  if (
+    nameInput.value.length < 3 ||
+    nameInput.value.length > 50 ||
+    cognomInput.value.length < 3 ||
+    cognomInput.value.length > 50
+  ) {
+    // displayError('Els camps Nom i cognom han de tenir una llaragade entre 3 i 50 caràcters.');
+    alert(
+      "Els camps Nom i cognom han de tenir una llaragade entre 3 i 50 caràcters."
+    );
+  }
+}
 
 // Si no saps com utilitzar aquestes funcions, pots fer ús de missatges per consol o situar
 // els errors allà on consideris lògic
