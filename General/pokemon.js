d34614b9f9a08568088c61bab2ca06d3f83966a8
Eduard Iglesias <eduard.iglesias@cirvianum.cat>
2024-04-30 19:04:04 +0200

    final segurament

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,6 +1,7 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
 
-llista = document.querySelector("ul");
+const llista = document.querySelector("ul");
+const idInput = document.querySelector("#cerca");
 
 async function getPokemon(id = 0, name = "") {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -17,17 +18,27 @@ async function getPokemon(id = 0, name = "") {
   }
 }
 
-showPokemon();
-
-async function showPokemon() {
+async function showPokemon(id) {
   try {
-    const pokemon = await getPokemon(2);
+    const pokemon = await getPokemon(2); // id
 
-    console.log(pokemon.abilities);
+    // console.log(pokemon.abilities);
 
     pokemon.abilities.forEach((ability) => {
-      llista.innerHTML += `<li class='mb-2'> <h4 class='font-bol'>${ability.ability.name}</h4> <p>Descripció de l'habilitat</p> </li>`;
+      //no puc fer servir la funció async a dins del foreach
+      llista.innerHTML += `<li class='mb-2'> <h4 class='font-bol'>${ability.ability.name}</h4>`;
+      //   <p>Descripció de l'habilitat</p> </li>`;
     });
+
+    let abilitiesDesc;
+
+    for (const url in pokemon.abilities.ability[1]) {
+      console.log(url);
+      let newAbility = await getAbility(url);
+      abilitiesDesc.push(newAbility);
+    }
+
+    console.log(abilitiesDesc);
 
     // pokemon.abilities.ability.name;
   } catch (error) {
@@ -35,3 +46,22 @@ async function showPokemon() {
     return [];
   }
 }
+
+async function getAbility(url) {
+  const res = await fetch(url);
+  try {
+    if (!res.ok) {
+      throw new Error("API request failed");
+    }
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+idInput.addEventListener("input", (e) => {
+  showPokemon(e);
+});
